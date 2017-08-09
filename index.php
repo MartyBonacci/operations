6d75eba12d3f2c35238b277b93bc5f706d7672a4
Marty Bonacci <martybonacci@gmail.com>
2017-08-09 11:02:09 -0600

    created index and script files from codepen sandbox

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="author" content="Marty Bonacci">
+		<meta name="description" content="operations">
+
+
+
+		<!-- JavaScript file -->
+		<script type="text/javascript" src="js/script.js"></script>
+
+		<title>operations</title>
+	</head>
+	<body>
+		<header>
+			<p>Enter value</p>
+		</header>
+		<input id="firstNumber" type="number" oninput="test()"/>
+		<p id="first">firstValue?</p>
+	</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,7 @@
+var resultTwo = document.getElementById("first");
+
+
+var test = function() {
+	var entered = document.getElementById("firstNumber").value;
+	resultTwo.textContent = "1stValue " + entered;
+};
